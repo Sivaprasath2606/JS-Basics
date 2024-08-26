a183374eb91b9527e1ad02a31ccba097505eeb9b
Sivaprasath2606 <sivaprasath2606@gmail.com>
2024-08-26 10:52:32 +0530

    .

--- this.js
+++ this.js
@@ -1,31 +1,50 @@
 let laptop1 = {
-cpu : "i5",
-ram : "2gb",
-rom : "256gb",
-
-sysconfig: function(){
-    console.log(this.cpu, this.ram, this.rom);
-}
-};
-
-let laptop2 = {
-    cpu : "i7",
-    ram : "4gb",
-    rom : "512gb",
+    cpu : "i5",
+    ram : "2gb",
+    rom : "256gb",
     
     sysconfig: function(){
-        console.log(this.cpu, this.ram, this.rom)
+        console.log(this.cpu, this.ram, this.rom);
     }
-};
-
-    let laptop3 = {
-        cpu :"i9",
-        ram : "8gb",
-        rom : "1tb",
+    };
+    
+    let laptop2 = {
+        cpu : "i7",
+        ram : "4gb",
+        rom : "512gb",
         
         sysconfig: function(){
-            console.log(this.cpu, this.ram, this.rom);
+            console.log(this.cpu, this.ram, this.rom)
         }
     };
-
-        laptop2.sysconfig();
+    
+        let laptop3 = {
+            cpu :"i9",
+            ram : "8gb",
+            rom : "1tb",
+            
+            sysconfig: function(){
+                console.log(this.cpu, this.ram, this.rom);
+            }
+        };
+    
+            laptop2.sysconfig();
+    
+    
+    
+    //  asynchronous JavaScript code
+    
+    function func1(callback)
+    {
+        setTimeout(() =>{ console.log("Task1"),callback()}, 5000)
+    }
+    
+    function func2(){
+        console.log("Task2")
+        console.log("Task3")
+        console.log("Task4")
+        console.log("Task5")
+        
+    }
+    
+    func1(func2)
